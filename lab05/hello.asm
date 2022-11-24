36edb74a463a026e635e6da11d08a56e7fc64afe
Ekaterina Shchankina <1132226524@pfur.ru>
2022-11-24 20:30:43 +0300

    feat(main): add files lab-7

--- lab05/hello.asm
+++ lab05/hello.asm
@@ -1,16 +1,15 @@
 ; hello.asm
-SECTION .data ; Начало секции данных
-hello: DB 'Hello world!',10 ; 'Hello world!' плюс
-; символ перевода строки
-helloLen: EQU $-hello ; Длина строки hello
-SECTION .text ; Начало секции кода
+SECTION .data 
+hello: DB 'Hello world!',10 
+helloLen: EQU $-hello 
+SECTION .text 
 GLOBAL _start
-_start: ; Точка входа в программу
-mov eax,4 ; Системный вызов для записи (sys_write)
-mov ebx,1 ; Описатель файла '1' - стандартный вывод
-mov ecx,hello ; Адрес строки hello в ecx
-mov edx,helloLen ; Размер строки hello
+_start: 
+mov eax,4 
+mov ebx,1 
+mov ecx,hello 
+mov edx,helloLen 
 int 80h ; Вызов ядра
-mov eax,1 ; Системный вызов для выхода (sys_exit)
-mov ebx,0 ; Выход с кодом возврата '0' (без ошибок)
-int 80h ; Вызов ядра
+mov eax,1 
+mov ebx,0 
+int 80h 
